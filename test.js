89ea6f68233306c4ee8cc149734d7ed24e78e15d
aaccer <103036800+aaccer@users.noreply.github.com>
2022-06-01 17:21:23 +0800

    Add files via upload

--- test.js
+++ test.js
@@ -15,11 +15,11 @@
             "playUrl": "",
             "searchable": 1,
             "filterable": 1,
-            "ext": {
-                "header": {
+        "ext": {
+            "header": {
                     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
-                }
-                }
+            }
+        }
             }, {
                 "key": "push_agent",
                 "name": "推送",
